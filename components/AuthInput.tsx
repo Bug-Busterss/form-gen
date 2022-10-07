35f8e6a5b0cd1230eb00487838f51e17d5f826d1
Adnanarodiya <aiarodiya07@gmail.com>
2022-10-07 18:27:50 +0530

    added Login page

---
+++ components/AuthInput.tsx
@@ -0,0 +1,29 @@
+import { TextInputProps } from "@mantine/core";
+import { FC, useState } from "react";
+// import { useFloatingInput } from "../hooks/useFloatingInput";
+
+interface Props {
+  input: FC<TextInputProps>;
+  label: string;
+}
+
+export default function AuthInput({
+  input: Input,
+  label,
+  ...formProps
+}: Props & any) {
+  const [focused, setFocused] = useState(false);
+  //   const { classes } = useFloatingInput({ floating: focused });
+  return (
+    <Input
+      label={label}
+      required
+      //   classNames={classes}
+      onFocus={() => setFocused(true)}
+      onBlur={() => setFocused(false)}
+      mt="md"
+      size="lg"
+      {...formProps}
+    />
+  );
+}
---
+++ pages/login.tsx
@@ -0,0 +1,57 @@
+import {
+  Card,
+  Image,
+  Text,
+  Badge,
+  Button,
+  Group,
+  CardSection,
+  Center,
+  Container,
+  Paper,
+  Stack,
+  Anchor,
+  TextInput,
+  PasswordInput,
+} from "@mantine/core";
+import { IconLogin } from "@tabler/icons";
+import AuthInput from "../components/AuthInput";
+
+export default function MyLogin() {
+  return (
+    <Container>
+      <Center>
+        <Paper radius="md" p="md" withBorder>
+          <Text size="lg" weight={500}>
+            Login
+          </Text>
+          <Stack mt="md">
+            <AuthInput
+              input={TextInput}
+              label="Username"
+              //   {...form.getInputProps("username")}
+            />
+            <AuthInput
+              input={PasswordInput}
+              label="Password"
+              //   {...form.getInputProps("password")}
+            />
+          </Stack>
+          <Group position="apart" mt="md" grow>
+            <Button
+              rightIcon={<IconLogin size="18px" />}
+              variant="gradient"
+              gradient={{ from: "#AD1DEB", to: "#6E72FC" }}
+              type="submit"
+            >
+              Login
+            </Button>
+          </Group>
+        </Paper>
+      </Center>
+    </Container>
+  );
+}
+function getCurentUser(arg0: any) {
+  throw new Error("Function not implemented.");
+}
