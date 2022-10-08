c9f6f8d9f99cdf80ee5e070e588a187c6b122cc6
Uraniumking007 <Uraniumking007@users.noreply.github.com>
2022-10-08 09:49:30 +0530

    minor bug fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Eligibility from "../components/Eligibility";
 import Requiremnts from "../components/requrements";
 import MyButton from "../components/button";
-import { Text } from "@mantine/core";
+import { Anchor, Text } from "@mantine/core";
 
 const Home: NextPage = () => {
   return (
@@ -12,7 +12,9 @@ const Home: NextPage = () => {
       </Text>
       <Eligibility />
       <Requiremnts />
-      <MyButton name='Apply Online' />
+      <Anchor href='/formfill'>
+        <MyButton name='Apply Online' />
+      </Anchor>
     </div>
   );
 };
